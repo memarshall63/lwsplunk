27ec027919eefc43774112950f4a64a5c6c3bbb5
marshall <marshall@shadowarc.com>
2021-03-29 17:13:41 -0400

    Cleanup

--- LWSpInstall.ps1
+++ LWSpInstall.ps1
@@ -18,71 +18,49 @@ param (
     [Parameter(Mandatory=$true)][string]$type
 )
 
-# Establish some constants.
-$Config = @{
-    version = "8.1.3"
-    download_file_full = "splunk-8.1.3-63079c59e632-x64-release.msi"
-    download_path_full = "https://download.splunk.com/products/splunk/releases/#VERSION#/windows/#DOWNLOAD_FILE#"
-    installdir_full = "C:\Splunk_Full\"
+if ("full" -eq $type)
+{
+    $Config = @{
+        version = "8.1.3"
+        download_file = "splunk-8.1.3-63079c59e632-x64-release.msi"
+        download_path = "https://download.splunk.com/products/splunk/releases/#VERSION#/windows/#DOWNLOAD_FILE#"
+        installdir = "C:\Splunk\"
+        splunk_user = "splunk"
+    }
+}
+else {
+    $Config = @{
+        download_file = "splunkforwarder-8.1.3-63079c59e632-x64-release.msi"
+        download_path = "https://download.splunk.com/products/universalforwarder/releases/#VERSION#/windows/#DOWNLOAD_FILE#"
+        installdir = "C:\SplunkForwarder\"
+        splunk_user = "splunk"
+    }
+}    
 
-    download_file_uf = "splunkforwarder-8.1.3-63079c59e632-x64-release.msi"
-    download_path_uf = "https://download.splunk.com/products/universalforwarder/releases/#VERSION#/windows/#DOWNLOAD_FILE#"
-    installdir_uf = "C:\SplunkForwarder\"
-};
 Write-Information "Config constants:"
 $out_str = $Config | Out-String
 Write-Information $out_str
 
 $Version = $Config['version']
 
-Write-Information "Downloading..."
-#Write-Information $Config['FullLocation']
-#Write-Information $Config['This_Setting']
-#Write-Information $Config['OtherSetting1']
+Write-Output "Downloading...$Config['download_file']"
 
-#   Download Splunk from Splunk.com
-if ( "full" -eq $type)
-{
 #    wget "https://download.splunk.com/products/splunk/releases/8.1.3/windows/splunk-8.1.3-63079c59e632-x64-release.msi -outfile splunk-8.1.3-63079c59e632-x64-release.msi"  
-    $Config['download_path_full'] = $Config['download_path_full'].Replace("#VERSION#", $Version)
-    $Config['download_path_full'] = $Config['download_path_full'].Replace("#DOWNLOAD_FILE#", $Config['download_file_full'])
-    Write-Information $Config['download_path_full']
-    wget $Config['download_path_full'] -outfile $Config['download_file_full'] 
-}
-else {
-    if ("uf" -eq $type)
-    {
-        $Config['download_path_uf'] = $Config['download_path_uf'].Replace("#VERSION#", $Version)
-        $Config['download_path_uf'] = $Config['download_path_uf'].Replace("#DOWNLOAD_FILE#", $Config['download_file_uf'])
-        Write-Information $Config['download_path_uf']
-        wget $Config['download_path_uf'] -outfile $Config['download_file_uf'] 
-    }
-    else {
-        Write-Error "Invalid Type argument:  must be 'full' or 'uf'."
-    }
-}
+$Config['download_path'] = $Config['download_path'].Replace("#VERSION#", $Version)
+$Config['download_path'] = $Config['download_path'].Replace("#DOWNLOAD_FILE#", $Config['download_file'])
+Write-Information $Config['download_path']
+Invoke-WebRequest $Config['download_path'] -outfile $Config['download_file'] 
 
-
-#
-#Install Splunk
-if ("full" -eq $type)
-{
-    $install_file = $Config['download_file_full']
-    $install_dir = $Config['installdir_full']
-}
-
-if ("uf" -eq $type)
-{
-    $install_file = $Config['download_file_uf']
-    $install_dir = $Config['installdir_uf']
-}
+$install_file = $Config['download_file']
+$install_dir = $Config['installdir']
+$install_user = $Config['splunk_user']
 
 #Create the install directory - otherwise msi fails cuz can't open log.
 if (!(Test-Path $install_dir))
 {
     Write-Information "Creating..."
     Write-Information $install_dir
-        New-Item -Path $install_dir -ItemType directory | Out-Null
+    New-Item -Path $install_dir -ItemType directory | Out-Null
 }
 
 $DateStamp = get-date -Format yyyyMMddTHHmmss
@@ -91,9 +69,9 @@ $MSIArguments =
     "/i",
     $install_file,
     "AGREETOLICENSE=yes",
-    "INSTALLDIR='$install_dir'",
-    "SPLUNKUSERNAME=admin",
-    "SPLUNKPASSWORD=splunkadmin",
+    "INSTALLDIR=$install_dir",
+    "SPLUNKUSERNAME=$install_user",
+    "SPLUNKPASSWORD=$install_user",
     "SERVICESTARTTYPE=manual",
     "LAUNCHSPLUNK=0",
     "/l*v",
@@ -101,10 +79,10 @@ $MSIArguments =
     "/quiet"
 
   
-Write-Information "Starting Installation..."
 $out_str = $MSIArguments | Out-String
 Write-Information "MSI Arguments:"
 Write-Information $out_str
+Write-Output "Starting Installation..."
 Start-Process -FilePath "msiexec" -ArgumentList $MSIArguments -Wait -NoNewWindow
-Write-Information "Completed."
+Write-Output "Completed."
 
